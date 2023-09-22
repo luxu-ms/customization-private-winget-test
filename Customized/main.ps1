cf391f5afef61687127602db733ff1c00cd3bf14
Lyle Xu <chanxl009@gmail.com>
2023-09-22 15:45:54 +0800

    refine script

--- Customized/main.ps1
+++ Customized/main.ps1
@@ -4,12 +4,13 @@ param (
 )
 
 $CustomizationScriptsDir = "C:\DevBoxCustomizations"
-$RunAsUserScript = "runAsUser.ps1"
-$RunAsUserScriptPath = "$($CustomizationScriptsDir)\$($RunAsUserScript)"
+$RunAsUserAppendScript = "runAsUser-append.ps1"
+$RunAsUserScriptPath = "$($CustomizationScriptsDir)\$($RunAsUserAppendScript)"
 if(!(Test-Path -Path $CustomizationScriptsDir)){
     New-Item $CustomizationScriptsDir -type directory
 }
 Add-Content -Path $RunAsUserScriptPath -Value "
 winget source remove -n winget
 winget source remove -n msstore
-winget source add -n WinGetRest $LocalRepoURL -t Microsoft.Rest"
+winget source add -n WinGetRest $LocalRepoURL -t Microsoft.Rest
+"
--- winget/main.ps1
+++ winget/main.ps1
@@ -14,17 +14,28 @@ param (
 $CustomizationScriptsDir = "C:\DevBoxCustomizations"
 $LockFile = "lockfile"
 $RunAsUserScript = "runAsUser.ps1"
+$RunAsUserAppendScript = "runAsUser-append.ps1"
 $CleanupScript = "cleanup.ps1"
 $RunAsUserTask = "DevBoxCustomizations"
 $CleanupTask = "DevBoxCustomizationsCleanup"
 
+if (!(Test-Path -PathType Container $CustomizationScriptsDir)) {
+    New-Item -Path $CustomizationScriptsDir -ItemType Directory
+}
+
+Copy-Item "./$($RunAsUserScript)" -Destination $CustomizationScriptsDir
+$RunAsUserAppendScriptPath = "$($CustomizationScriptsDir)\$($RunAsUserAppendScript)"
+$RunAsUserScriptPath = "$($CustomizationScriptsDir)\$($RunAsUserScript)"
+if(Test-Path -Path $RunAsUserAppendScriptPath){
+    $From = Get-Content -Path $RunAsUserAppendScriptPath
+    Add-Content -Path $RunAsUserScriptPath -Value $From
+}
+
+Copy-Item "./$($CleanupScript)" -Destination $CustomizationScriptsDir
+
 function SetupScheduledTasks {
-    if (!(Test-Path -PathType Container $CustomizationScriptsDir)) {
-        New-Item -Path $CustomizationScriptsDir -ItemType Directory
-        New-Item -Path "$($CustomizationScriptsDir)\$($LockFile)" -ItemType File
-        Copy-Item "./$($RunAsUserScript)" -Destination $CustomizationScriptsDir
-        Copy-Item "./$($CleanupScript)" -Destination $CustomizationScriptsDir
-    }
+
+    New-Item -Path "$($CustomizationScriptsDir)\$($LockFile)" -ItemType File
 
     # Reference: https://learn.microsoft.com/en-us/windows/win32/taskschd/task-scheduler-objects
     $ShedService = New-Object -comobject "Schedule.Service"
@@ -59,7 +70,7 @@ function SetupScheduledTasks {
 
     $Action = $Task.Actions.Create(0)
     $Action.Path = "C:\Program Files\PowerShell\7\pwsh.exe"
-    $Action.Arguments = "-MTA -Command $($CustomizationScriptsDir)\$($RunAsUserScript)"
+    $Action.Arguments = "-MTA -Command $RunAsUserScriptPath"
 
     $TaskFolder = $ShedService.GetFolder("\")
     $TaskFolder.RegisterTaskDefinition("$($RunAsUserTask)", $Task , 6, "Users", $null, 4)
@@ -67,7 +78,7 @@ function SetupScheduledTasks {
 
 function getInstallCommand($package, $override) {
     $installCommand = ""
-    if ($Package) {
+    if ($package) {
         $installCommand = "winget install $($package)"
         if(-not ([string]::IsNullOrEmpty($override))) {
             $installCommand += " --override '$override'"
@@ -80,7 +91,7 @@ function getInstallCommand($package, $override) {
 $installCommand = getInstallCommand($Package, $Override)
 
 if (($RunAsUser -eq "true") -and ($installCommand -ne "")) {
-    Add-Content -Path "$($CustomizationScriptsDir)\$($RunAsUserScript)" -Value $installCommand
+    Add-Content -Path $RunAsUserScriptPath -Value $installCommand
 }
 
 # TODO only need to setup scheduled tasks if running as user
